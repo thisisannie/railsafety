864d0d774b05cb81cced5fb463b665cc6bb777c3
Annie Kyles <anniek@federation.net.nz>
2023-03-08 13:59:17 +1300

    Init

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rail Safety</title>
+    <meta name="description" content="On behalf of us all at Federation, thanks for the opportunity to share our thinking on how we can help kiwis stay safe around rail crossings. We’d love the opportunity to further collaborate with you to build an engaging and effective behavioural change platform for Rail Safety Week 2023 (and way beyond!).
+  ">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="left">
+                <div class="logo logo-kr">
+                    <img src="./images/logos/kiwirail.png" alt="Kiwirail Logo" title="Kiwirail">
+                </div>
+                <div class="logo logo-tsf">
+                    <img src="./images/logos/tracksafe-foundation.svg" alt="TrackSafe Foundation Logo" title="TrackSafe Foundation">
+                </div>
+            </div>
+            <div class="right">
+                <a class="logo logo-fed" href="https://federation.net.nz" target="_blank">
+                    <img src="./images/logos/federation-logo-white.svg" alt="Federation logo" title="Federation">
+                </a>
+            </div>
+        </header>
+        <section>
+            <div class="content">
+                <h1>Kia ora</h1>
+                <p>On behalf of us all at Federation, thanks for the opportunity to share our thinking on how we can help Kiwis stay safe around rail crossings.</p>
+                <p>We’d love the opportunity to further collaborate with you to build an engaging and effective behavioural change platform for Rail Safety Week 2023 (and way beyond!).</p>
+                <p>We look forward to talking more.</p>
+                <p>Ngā mihi nui</p>
+                <a href="./pdfs/test.pdf" class="button">Download Presentation [update pdf]</a>
+            </div>            
+        </section>
+    </div>
+    <?php /* <script src="./js/custom.js"></script> */?>    
+</body>
+</html>
